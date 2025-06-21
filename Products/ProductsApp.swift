de5d11dfb4703679fccc0b633e4089a096036ef0
ktw11 <ktw1897@naver.com>
2025-06-21 10:57:29 +0900

    init

---
+++ Products/ProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsApp.swift
+//  Products
+//
+//  Created by 공태웅 on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
